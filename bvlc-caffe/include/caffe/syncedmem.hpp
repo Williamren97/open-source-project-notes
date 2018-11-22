7c9766287a92cdb3521b9fc8fe39653fd2042271
Jianming Chen <398342899@qq.com>
2018-11-22 21:50:58 +0800

    [bvlc-caffe] syncedmem.

--- bvlc-caffe/include/caffe/syncedmem.hpp
+++ bvlc-caffe/include/caffe/syncedmem.hpp
@@ -19,6 +19,10 @@ namespace caffe {
 inline void CaffeMallocHost(void** ptr, size_t size, bool* use_cuda) {
 #ifndef CPU_ONLY
   if (Caffe::mode() == Caffe::GPU) {
+    // 在主机端分配页锁定内存。
+    // 对于cuda在主机端的内存有分可分页内存(pageable memroy)和页锁定内存(page-lock或pinned)。
+    // 页锁定后，主机的操作系统将不会对这块内存进行分页和交换操作，确保该内存始终驻留在物理内存中。
+    // cuda可跟踪页锁定内存的物理地址，通过“直接内存访问(Direct Memory Access，DMA)”直接在主机和GPU之间复制数据，速率更快。
     CUDA_CHECK(cudaMallocHost(ptr, size));
     *use_cuda = true;
     return;
@@ -36,6 +40,7 @@ inline void CaffeMallocHost(void** ptr, size_t size, bool* use_cuda) {
 inline void CaffeFreeHost(void* ptr, bool use_cuda) {
 #ifndef CPU_ONLY
   if (use_cuda) {
+    // 对应cudaMallocHost。
     CUDA_CHECK(cudaFreeHost(ptr));
     return;
   }
@@ -78,11 +83,18 @@ class SyncedMemory {
 
   void to_cpu();
   void to_gpu();
+  // 指向CPU端内存的指针。
   void* cpu_ptr_;
+  // 指向GPU端内存的指针。
   void* gpu_ptr_;
+  // 内存大小。
   size_t size_;
+  // 用于标记正使用的数据是在哪一端。
+  // 分{未初始化，在CPU端，在GPU端，同步的}四种状态。
   SyncedHead head_;
+  // 用于标记是否已经存在由自己分配的数据，设置数据时要用到。
   bool own_cpu_data_;
+  // 用于标记该CPU端的内存分配是否是通过GPU分配的页锁定内存。
   bool cpu_malloc_use_cuda_;
   bool own_gpu_data_;
   int device_;
--- bvlc-caffe/src/caffe/syncedmem.cpp
+++ bvlc-caffe/src/caffe/syncedmem.cpp
@@ -35,16 +35,19 @@ SyncedMemory::~SyncedMemory() {
   }
 #endif  // CPU_ONLY
 }
-
+// 推送数据到CPU端。
 inline void SyncedMemory::to_cpu() {
   check_device();
+  // 需要先用标记的head_来判断当前使用的数据是在哪一端。
   switch (head_) {
+  // 若未初始化，则给CPU端分配页锁定内存。
   case UNINITIALIZED:
     CaffeMallocHost(&cpu_ptr_, size_, &cpu_malloc_use_cuda_);
     caffe_memset(size_, 0, cpu_ptr_);
     head_ = HEAD_AT_CPU;
     own_cpu_data_ = true;
     break;
+  // 如果当前数据是在GPU端，则拷贝过去。
   case HEAD_AT_GPU:
 #ifndef CPU_ONLY
     if (cpu_ptr_ == NULL) {
@@ -57,12 +60,14 @@ inline void SyncedMemory::to_cpu() {
     NO_GPU;
 #endif
     break;
+  // 若本身就在CPU端或是同步的，则不做处理。
   case HEAD_AT_CPU:
   case SYNCED:
     break;
   }
 }
 
+// 推送数据到GPU端，与上面to_cpu类似。
 inline void SyncedMemory::to_gpu() {
   check_device();
 #ifndef CPU_ONLY
@@ -90,23 +95,31 @@ inline void SyncedMemory::to_gpu() {
 #endif
 }
 
+// 获取CPU端数据。
 const void* SyncedMemory::cpu_data() {
+  // 先检查设备，看当前使用的GPU与开始时参数设置的GPU是否一致。
   check_device();
+  // 推送数据到CPU端，若本身就在CPU端的则不会做什么操作。
   to_cpu();
   return (const void*)cpu_ptr_;
 }
 
+// 设置CPU端数据。
 void SyncedMemory::set_cpu_data(void* data) {
   check_device();
+  // 输入指针不能为空。
   CHECK(data);
+  // 判断是否已经存在由自己控制的数据，若有则需要先释放掉。
   if (own_cpu_data_) {
     CaffeFreeHost(cpu_ptr_, cpu_malloc_use_cuda_);
   }
   cpu_ptr_ = data;
   head_ = HEAD_AT_CPU;
+  // 指针是直接赋过来的，内存由外面控制，内部无权释放，所以为false。
   own_cpu_data_ = false;
 }
 
+// 获取GPU端数据。
 const void* SyncedMemory::gpu_data() {
   check_device();
 #ifndef CPU_ONLY
@@ -118,6 +131,7 @@ const void* SyncedMemory::gpu_data() {
 #endif
 }
 
+// 设置GPU端数据，与上面set_cpu_data类似。
 void SyncedMemory::set_gpu_data(void* data) {
   check_device();
 #ifndef CPU_ONLY
@@ -133,6 +147,10 @@ void SyncedMemory::set_gpu_data(void* data) {
 #endif
 }
 
+// 获取CPU端数据的指针。
+// 比cpu_data()多一句head_ = HEAD_AT_CPU。
+// 意思是调用mutable_cpu_data()函数时，外部可以直接通过返回的指针来修改数据。
+// 而cpu_data()返回的指针内存则不可修改。
 void* SyncedMemory::mutable_cpu_data() {
   check_device();
   to_cpu();
@@ -140,6 +158,7 @@ void* SyncedMemory::mutable_cpu_data() {
   return cpu_ptr_;
 }
 
+// 获取GPU端数据的指针，与上面mutable_cpu_data()类似。
 void* SyncedMemory::mutable_gpu_data() {
   check_device();
 #ifndef CPU_ONLY
@@ -153,6 +172,7 @@ void* SyncedMemory::mutable_gpu_data() {
 }
 
 #ifndef CPU_ONLY
+// 异步将数据从CPU端拷贝到GPU端。
 void SyncedMemory::async_gpu_push(const cudaStream_t& stream) {
   check_device();
   CHECK(head_ == HEAD_AT_CPU);
@@ -161,12 +181,15 @@ void SyncedMemory::async_gpu_push(const cudaStream_t& stream) {
     own_gpu_data_ = true;
   }
   const cudaMemcpyKind put = cudaMemcpyHostToDevice;
+  // 异步拷贝函数。
   CUDA_CHECK(cudaMemcpyAsync(gpu_ptr_, cpu_ptr_, size_, put, stream));
   // Assume caller will synchronize on the stream before use
+  // 做标记，在使用数据的时候需要确保该数据已经拷贝完毕。
   head_ = SYNCED;
 }
 #endif
 
+// 检查当前使用的GPU与参数设置的GPU是否一致。
 void SyncedMemory::check_device() {
 #ifndef CPU_ONLY
 #ifdef DEBUG

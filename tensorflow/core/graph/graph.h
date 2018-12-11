26e63c74b8b15dc7333f66c561c9d0e7c80aec82
Jianming Chen <398342899@qq.com>
2018-12-11 21:27:27 +0800

    [tensorflow] graph.

--- tensorflow/core/graph/graph.h
+++ tensorflow/core/graph/graph.h
@@ -143,6 +143,7 @@ class Node {
   bool IsSource() const { return id() == 0; }
   bool IsSink() const { return id() == 1; }
   // Anything other than the special Source & Sink nodes.
+  // 除了特殊的源Source和接收Sink节点以外的任何节点。
   bool IsOp() const { return id() > 1; }
 
   // Node class helpers
@@ -467,7 +468,7 @@ class Graph {
   // The graph can hold ops found in registry. `registry`s lifetime must be at
   // least that of the constructed graph's.
   //
-  // 以一个SOURCE、一个SINK节点和一条SOURCE到SINK的边，去构建一个计算图。
+  // 以一个源SOURCE、一个接收SINK节点和一条源节点到接收节点的边，去构建一个计算图。
   // 该计算图可以保存registry中找到的ops。
   // registry的生命周期必须长于构造的计算图的生命周期。
   explicit Graph(const OpRegistryInterface* registry);
@@ -532,27 +533,40 @@ class Graph {
   // `allow_duplicates` is true.
   // TODO(skyewm): // TODO(skyewm): allow_duplicates is needed only by
   // graph_partition.cc. Figure out if we can do away with it.
+  // 
+  // 添加连接source和dest的control edge(该edge没有数据流)。
+  // 如果dest的NodeDef缺少相应的控件(control)输入，则添加控件输入。
+  //
+  // 如果这样的control edge已经存在，并且allow_duplicate为false，
+  // 则不添加任何edge，函数返回nullptr。否则，将无条件创建并返回edge。
+  // 如果allow_duplicate为true，则不更新节点定义。
   const Edge* AddControlEdge(Node* source, Node* dest,
                              bool allow_duplicates = false);
 
   // Removes edge from the graph. Does not update the destination node's
   // NodeDef.
   // REQUIRES: The edge must exist.
+  // 从图形中移除edge。不更新目标节点的NodeDef。
   void RemoveEdge(const Edge* edge);
 
   // Removes control edge `edge` from the graph. Note that this also updates
   // the corresponding NodeDef to reflect the change.
   // REQUIRES: The control edge must exist.
+  // 从图形中移除control edge e。需要注意的是，这会更新相应的NodeDef以反映该更改。
   void RemoveControlEdge(const Edge* e);
   // Updates the input to a node.  The existing edge to `dst` is removed and an
   // edge from `new_src` to `dst` is created. The NodeDef associated with `dst`
   // is also updated.
+  // 更新节点的输入。删除到dst的现有edge，并创建从new_src到dst的edge。
+  // 与dst关联的NodeDef也会更新。
   Status UpdateEdge(Node* new_src, int new_src_index, Node* dst, int dst_index);
 
   // Adds the function and gradient definitions in `fdef_lib` to this graph's op
   // registry. Ignores duplicate functions, and returns a bad status if an
   // imported function differs from an existing function or op with the same
   // name.
+  // 将fdef_lib中的函数和梯度定义添加到图的op注册表中。
+  // 忽略重复函数，如果导入的函数与现有函数或op不同，但名字却相同的，则返回错误状态。
   Status AddFunctionLibrary(const FunctionDefLibrary& fdef_lib);
 
   // The number of live nodes in the graph.
@@ -561,9 +575,14 @@ class Graph {
   // smaller than num_node_ids(). If one needs to create an array of
   // nodes indexed by node ids, num_node_ids() should be used as the
   // array's size.
+  // 
+  // 计算图中活动节点的数量。
+  // 因为节点可以从图中删除，所以num_nodes()通常小于num_node_ids()。
+  // 如果需要创建按节点id索引的节点数组，则应该使用num_node_ids()作为数组的大小。
   int num_nodes() const { return num_nodes_; }
 
   // The number of live nodes in the graph, excluding the Source and Sink nodes.
+  // 计算图图中活动节点的数量，不包括源节Source点和接收Sink节点
   int num_op_nodes() const {
     DCHECK_GE(num_nodes_, 2);
     return num_nodes_ - 2;
@@ -575,12 +594,15 @@ class Graph {
   // smaller than num_edge_ids(). If one needs to create an array of
   // edges indexed by edge ids, num_edge_ids() should be used as the
   // array's size.
+  // 与num_nodes()类似，针对edges。
   int num_edges() const { return num_edges_; }
 
   // Serialize the nodes starting at `from_node_id` to a GraphDef.
+  // 将从from_node_id开始的节点序列化为GraphDef。
   void ToGraphDefSubRange(GraphDef* graph_def, int from_node_id) const;
 
   // Serialize to a GraphDef.
+  // 将所有节点序列化为GraphDef，直接调用ToGraphDefSubRange(graph_def, 0);
   void ToGraphDef(GraphDef* graph_def) const;
 
   // This version can be called from debugger to inspect the graph content.
@@ -588,42 +610,52 @@ class Graph {
   //
   // Note: We do not expose a DebugString() API, since GraphDef.DebugString() is
   // not defined in some TensorFlow builds.
+  // 检查图形内容.
   GraphDef ToGraphDefDebug() const;
 
   // Generate new node name with the specified prefix that is unique
   // across this graph.
+  // 生成具有指定前缀的新节点名，该前缀在此图中是惟一的。
   string NewName(StringPiece prefix);
 
   // Access to the list of all nodes.  Example usage:
   //   for (Node* node : graph.nodes()) { ... }
+  // 访问所有节点的列表
   gtl::iterator_range<NodeIter> nodes() const;
 
   // Access to the list of all nodes, excluding the Source and Sink nodes.
+  // 访问所有节点的列表，除了源Source和接收Sink节点
   gtl::iterator_range<NodeIter> op_nodes() const;
 
   // Returns one more than the maximum id assigned to any node.
+  // 返回比分配给任何节点的最大id多一的值。
   int num_node_ids() const { return nodes_.size(); }
 
   // Returns the node associated with an id, or nullptr if no node
   // with that id (the node with that id was removed and the id has
   // not yet been re-used). *this owns the returned instance.
   // REQUIRES: 0 <= id < num_node_ids().
+  // 根据id号找节点
   Node* FindNodeId(int id) const { return nodes_[id]; }
 
   // Returns one more than the maximum id assigned to any edge.
+  // 返回比分配给任何edges的最大id多一的值。
   int num_edge_ids() const { return edges_.size(); }
 
   // Returns the Edge associated with an id, or nullptr if no edge
   // with that id (the node with that id was removed and the id has
   // not yet been re-used). *this owns the returned instance.
   // REQUIRES: 0 <= id < num_node_ids().
+  // 根据id号找edge
   const Edge* FindEdgeId(int id) const { return edges_[id]; }
 
   // Access to the set of all edges.  Example usage:
   //   for (const Edge* e : graph.edges()) { ... }
+  // 访问所有edges。
   GraphEdgesIterable edges() const { return GraphEdgesIterable(edges_); }
 
   // The pre-defined nodes.
+  // 预定义的节点，在构建计算图时，最先添加的就是源和接收节点，id分别为0和1.
   enum { kSourceId = 0, kSinkId = 1 };
   Node* source_node() const { return FindNodeId(kSourceId); }
   Node* sink_node() const { return FindNodeId(kSinkId); }
@@ -636,6 +668,8 @@ class Graph {
     DCHECK_LT(index, static_cast<int>(device_names_.size()));
   }
 
+  // 确保device_name出现在设备名称表中，并返回该设备名称的索引。
+  // 该索引是稳定的，可以在调用Node::set_assigned_device_name_index()时使用。
   int InternDeviceName(const string& device_name);
 
   const string& get_assigned_device_name(const Node& node) const {
@@ -665,6 +699,8 @@ class Graph {
   // Create and return a new WhileContext owned by this graph. This is called
   // when a new while loop is created. `frame_name` must be unique among
   // WhileContexts in this graph.
+  // 创建并返回一个新的WhileContext，归该计算图所有。
+  // frame_name在此计算图的WhileContexts中必须是惟一的。
   Status AddWhileContext(StringPiece frame_name, std::vector<Node*> enter_nodes,
                          std::vector<Node*> exit_nodes,
                          OutputTensor cond_output,
@@ -680,11 +716,16 @@ class Graph {
   // created.
   //
   // Ownership of the returned Node is not transferred to caller.
+  //
+  // 如果cost_node不为空，那么cost accounting(在CostModel中)将与
+  // 该节点相关联，而不是与正在创建的新节点相关联。
+  // 返回节点的所有权不会转移到调用方。
   Node* AllocateNode(std::shared_ptr<NodeProperties> props,
                      const Node* cost_node);
   void ReleaseNode(Node* node);
 
   // Registry of all known ops, including functions.
+  // 注册所有已知的ops，包括functions。
   FunctionLibraryDefinition ops_;
 
   // GraphDef versions
@@ -695,23 +736,29 @@ class Graph {
 
   // Map from node ids to allocated nodes.  nodes_[id] may be nullptr if
   // the node with that id was removed from the graph.
+  // 从节点id到节点的映射表，如果节点nodes_[id]从计算图中被删除了，那么其值有可能为空。
   std::vector<Node*> nodes_;
 
   // Number of nodes alive.
+  // 活跃节点的数量。
   int64 num_nodes_ = 0;
 
   // Map from edge ids to allocated edges.  edges_[id] may be nullptr if
   // the edge with that id was removed from the graph.
+  // 从edge的id到edge的映射表，如果edges_[id]从计算图中被删除了，那么其值有可能为空。
   std::vector<Edge*> edges_;
 
   // The number of entries in edges_ that are not nullptr.
+  // 活跃edge的数量。
   int num_edges_ = 0;
 
   // Allocated but free nodes and edges.
+  // 分配了，但后面被释放了的节点和边。
   std::vector<Node*> free_nodes_;
   std::vector<Edge*> free_edges_;
 
   // For generating unique names.
+  // 用于生成唯一的名称。
   int name_counter_ = 0;
 
   // In most graphs, the number of unique values used for the
@@ -722,8 +769,15 @@ class Graph {
   // well a map (device_names_map_) from unique strings to indices within the
   // unique string table.
   //
+  // 在大多数图中，用于Node::assigned_device_name()属性的惟一值非常少。
+  // 如果计算图很大，那么这种值的重复会消耗大量内存。相反，我们使用一个外部的表
+  // 去表示相同的信息，该表由惟一字符串(device_names_)组成的向量以及从惟一字符串
+  // 到惟一字符串表中的索引的映射(device_names_map_)。
+  //
   // The InternDeviceName() method handles adding a new entry into the table,
   // or locating the index of an existing entry.
+  //
+  ////////// InternDeviceName()方法处理向表中添加新条目，或定位现有条目的索引。
   //
   // The fact that Node::assigned_device_name() is implemented using an
   // interning table is intentionally public.  This allows algorithms that
